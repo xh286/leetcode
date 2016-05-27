64a5732a738ca310bbffe61e876cfc48c3742019
xh286 <xh286@nyu.edu>
2016-05-26 21:14:37 -0400

    [344. Reverse String][Accepted]committed by LeetCode Extension

--- 344-Reverse-String/solution.cpp
+++ 344-Reverse-String/solution.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     string reverseString(string s) {
-        
+        string t;
+        t.resize(s.length()); // same length as s
+        std::string::reverse_iterator iter_in = s.rbegin(); // reverse iterator
+        std::string::iterator iter_out = t.begin();
+        std::string::reverse_iterator iter_end = s.rend();
+        while(iter_in != iter_end)
+        {
+            *iter_out = *iter_in;
+            iter_in++;
+            iter_out++;
+        }
+        return t;
     }
 };
