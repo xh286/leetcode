c7a82d9cec72cbccb2cf2a3ffb6d6d96fc8bf02a
xh286 <xh286@nyu.edu>
2016-05-27 12:25:46 -0400

    [136. Single Number][Accepted]committed by LeetCode Extension

--- 136-Single-Number/solution.cpp
+++ 136-Single-Number/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         // A very special problem. Inputs are integers, and appear twice. x xor x == 0, x xor 0 == x.
-        size_t n = nums.length();
+        size_t n = nums.size();
         int output = 0;
         for(size_t i = 0; i < n; i++)
         {
