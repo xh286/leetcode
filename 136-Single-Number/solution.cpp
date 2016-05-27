6ce2f7f33b151fe834e745cb4f1b53e2beb5bcae
xh286 <xh286@nyu.edu>
2016-05-27 12:25:27 -0400

    [136. Single Number][Compile Error]committed by LeetCode Extension

---
+++ 136-Single-Number/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // A very special problem. Inputs are integers, and appear twice. x xor x == 0, x xor 0 == x.
+        size_t n = nums.length();
+        int output = 0;
+        for(size_t i = 0; i < n; i++)
+        {
+            output ^= nums[i];
+        }
+        return output;
+    }
+};
