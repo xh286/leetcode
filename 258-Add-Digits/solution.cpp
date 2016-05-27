d18bdd2fd75fdae400ddfa382926cb7a3daf0291
xh286 <xh286@nyu.edu>
2016-05-27 12:49:48 -0400

    [258. Add Digits][Accepted]committed by LeetCode Extension

--- 258-Add-Digits/solution.cpp
+++ 258-Add-Digits/solution.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int output = num;
-        while(output >=10)
-        {
-            // add digits
-            int a = output;
-            int b = 0;
-            while(a != 0)
-            {
-                b += a - (a/10)*10;
-                a /= 10;
-            }
-            output = b;
-        }
-        return output;
+        return (num - (num-1)/9*9);
     }
 };
