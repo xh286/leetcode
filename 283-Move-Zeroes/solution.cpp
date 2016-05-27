85eec0a23ba357c9100850455d2cc100d3b8435f
xh286 <xh286@nyu.edu>
2016-05-27 16:55:06 -0400

    [283. Move Zeroes][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 283-Move-Zeroes/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        size_t n = nums.size();
+        size_t i = 0; // index
+        size_t c = 0; // count of zeros
+        while(i < n)
+        {
+            if(nums[i] == 0) //consecutive zeros
+            {
+                c++;
+            }
+            else // (nums[i] != 0) // consecutive non-zeros
+            {
+                nums[i-c] = nums[i];
+            }
+            i++;
+        }
+        // Replace last c entries with zeros
+        i = n - c;
+        while(i < n)
+        {
+            nums[i] = 0;
+        }
+    }
+};
