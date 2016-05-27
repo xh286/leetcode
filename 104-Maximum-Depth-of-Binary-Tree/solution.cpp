c79b627cce3d67b166727e7f4e3c6f39d31bd62b
xh286 <xh286@nyu.edu>
2016-05-27 14:32:23 -0400

    [104. Maximum Depth of Binary Tree][Accepted]committed by LeetCode Extension

--- 104-Maximum-Depth-of-Binary-Tree/solution.cpp
+++ 104-Maximum-Depth-of-Binary-Tree/solution.cpp
@@ -13,17 +13,14 @@ public:
         /* post-order DFS, stack keeps the path from root to current node. Return maximum length of stack. */
         std::vector<TreeNode *> dfs_stack;
         TreeNode * subtree = root;
-        bool subtree_popped = false; // with post-order DFS, when going up, we need to make sure the left & right children of a popped node does not get traversed again.
+        bool subtree_popped = false; // with post-order DFS, parents are only popped after both left/right are traversed. We need to make sure the left & right children of a popped node does not get traversed again.
         int max_depth = 0;
         while(true)
         {
             if(subtree && !subtree_popped) // subtree to traverse is not empty, and not popped, push into stack.
             {
                 dfs_stack.push_back(subtree);
-                if(dfs_stack.size()>max_depth)
-                {
-                    max_depth = dfs_stack.size();
-                }
+
                 subtree = subtree->left;
                 subtree_popped = false;
                 continue;
@@ -35,7 +32,20 @@ public:
             }
             // subtree is now child of stack top. Are we returning from left subtree, or right subtree? 
             TreeNode * subtree_parent = dfs_stack.back();
-            if(subtree == subtree_parent->left && subtree != subtree_parent->right) // returning from left subtree, and stack top is not leaf. Otherwise infinite loop.
+            // Is stack top leaf?
+            if(subtree_parent->left == NULL && subtree_parent->right == NULL)
+            {
+                if(dfs_stack.size()>max_depth)
+                {
+                    max_depth = dfs_stack.size();
+                }
+                subtree = subtree_parent;
+                dfs_stack.pop_back();
+                subtree_popped = true;
+                continue;
+            }
+            // returning from left/right subtree of non-leaf node
+            if(subtree == subtree_parent->left) // returning from left subtree.
             {
                 subtree = subtree_parent->right;
                 subtree_popped = false;
