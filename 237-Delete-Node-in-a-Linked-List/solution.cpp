df0ac409abe99e2a56156b03c4490c27463cf952
xh286 <xh286@nyu.edu>
2016-05-27 17:44:26 -0400

    [237. Delete Node in a Linked List][Compile Error]committed by LeetCode Extension

---
+++ 237-Delete-Node-in-a-Linked-List/solution.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        assert(node->next != NULL);
+        node->val = node->next->val;
+        ListNote* p= node->next;
+        node->next = node->next->next;
+        free(p);
+    }
+};
