0edb364f776e24b8d331819df50107e204e14586
xh286 <xh286@nyu.edu>
2016-05-27 23:41:10 -0400

    [122. Best Time to Buy and Sell Stock II][Compile Error]committed by LeetCode Extension

---
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        size_t n = prices.size();
+        int entry_price = 0, exit_price = 0; 
+        int total_profit = 0;
+        bool has_long_position = false;
+        for(i = 0; i < n-1; i++)
+        {
+            if( (has_long_postion == false) && (prices[i+1] > prices[i]) ) // buy
+            {
+                entry_price = prices[i];
+                has_long_position = true;
+            }
+            else if( (has_long_postion == true) && (prices[i+1] < prices[i]) ) // sell
+            {
+                exit_price = prices[i];
+                has_long_position = false;
+                total_profit += exit_price - entry_price;
+            }
+        }
+        // Now i = n-1.
+        if(has_long_position == true) // last day, record paper profit too
+        {
+            exit_price = prices[i];
+            has_long_position = false;
+            total_profit += exit_price - entry_price;
+        }
+        return total_profit;
+    }
+};
