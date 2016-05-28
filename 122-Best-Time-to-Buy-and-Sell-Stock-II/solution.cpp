2204f59a2e23872543b266182b4689c777693670
xh286 <xh286@nyu.edu>
2016-05-27 23:49:02 -0400

    [122. Best Time to Buy and Sell Stock II][Accepted]committed by LeetCode Extension

--- 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.cpp
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.cpp
@@ -6,8 +6,12 @@ public:
         int total_profit = 0;
         bool has_long_position = false;
         int i = 0;
-        for(i = 0; i < n-1; i++)
+        for(i = 0; i < n-1; i++)// note: size_t i & n can fail this loop condition check when n == 0.
         {
+            // Reason to buy is can sell at higher price later. 
+            // Because of no limitation on frequency, just look ahead one step is enough.
+            // Rule is: if increase next day, then buy previous day; if decrease next day, then sell previous day.
+            // Constrained: if have long position, no buying; if no longer position, no selling.
             if( (has_long_position == false) && (prices[i+1] > prices[i]) ) // buy
             {
                 entry_price = prices[i];
