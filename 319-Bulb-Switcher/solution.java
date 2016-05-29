5440322647ef357da6f5b1a27acb2e3cd8ff782d
xh286 <xh286@nyu.edu>
2016-05-29 15:28:06 -0400

    [319. Bulb Switcher][Compile Error]committed by LeetCode Extension

---
+++ 319-Bulb-Switcher/solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int bulbSwitch(int n) {
+        // naive solution would be O(n^2) time, O(1) space.
+        // Could do better. Only requires return a number.
+        // Perfect squares are odd factors, e.g. 9 has 1, 3, 9 as factors. So only perfect squares get set in the end.
+        return sqrt(n);
+        
+    }
+}
