9823d7659182d4ed94a63a33bb73521125cf1d80
xh286 <xh286@nyu.edu>
2016-05-29 19:46:17 -0400

    [318. Maximum Product of Word Lengths][Compile Error]committed by LeetCode Extension

--- 318-Maximum-Product-of-Word-Lengths/solution.cpp
+++ 318-Maximum-Product-of-Word-Lengths/solution.cpp
@@ -4,20 +4,31 @@ public:
         // brute force solution as first step
         int n = words.size();
         int maxproduct = 0;
+        vector<int> l_words; // lengths of (original) words;
         vector<vector<int> > table(n, vector<int>(26,0));
+        vector<string> c_words(n); // converted words. IN each word, characters are unique and ascending.
         for(int i=0; i<n; i++)
         {
-            for(int k = words[i].size() - 1; k >= 0; k--)
+            l_words[i] = words[i].size();
+            for(int k = 0; k < l_words[i]; k++)
                 table[i][words[i][k] - 'a']++;
+            for(int k = 0; k < 26; k++)
+                if(table[i][k] > 0)
+                    c_words[i].append('a'+k);
         }
+        
         for(int i=0; i<n; i++)
         {
             for(int j=i+1; j<n; j++)
             {// check collision 
                 bool collision = false;
-                for(int k = 0; k < 26; k++)
+                for(int p = 0, q = 0; p < c_words[i].length && q < c_words[j].length;)
                 {
-                    if(table[i][k] > 0 && table[j][k] > 0) // collision
+                    if(c_words[i][p] < c_words[j][q])
+                        p++;
+                    else if(c_words[i][p] > c_words[j][q])
+                        q++;
+                    else // (c_words[i][p] == c_words[j][q]) // collision
                     {
                         collision = true;
                         break;
@@ -25,7 +36,7 @@ public:
                 }
                 if(!collision)
                 {
-                    int product = words[i].size() * words[j].size();
+                    int product = l_words[i] * l_words[j];
                     if(product > maxproduct) 
                         maxproduct = product;
                 }
