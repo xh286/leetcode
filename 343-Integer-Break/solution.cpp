9ae2922442965537411d902a81b15f3154957bc9
xh286 <xh286@nyu.edu>
2016-05-29 15:39:31 -0400

    [343. Integer Break][Accepted]committed by LeetCode Extension

--- 343-Integer-Break/solution.cpp
+++ 343-Integer-Break/solution.cpp
@@ -5,7 +5,7 @@ public:
         // Proof: if n even, then break into two equal factors, n^2/4>n when n>4. If n odd, then (n+1)*(n-1)/4>n.
         if(n == 2) return 1;
         if(n == 3) return 2;
-        if(n == 4), return 4;
+        if(n == 4) return 4;
         int product = 1;
         while(n>=5)
         {
