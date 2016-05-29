189aa8b37238464e05b2334c200150b6c7721eb3
xh286 <xh286@nyu.edu>
2016-05-29 18:46:20 -0400

    [318. Maximum Product of Word Lengths][Accepted]committed by LeetCode Extension

--- 318-Maximum-Product-of-Word-Lengths/solution.cpp
+++ 318-Maximum-Product-of-Word-Lengths/solution.cpp
@@ -4,13 +4,14 @@ public:
         // brute force solution as first step
         int n = words.size();
         int maxproduct = 0;
-        //vector<vector<int> > table(n, vector<int>(26,0));
+        vector<vector<int> > table(n, vector<int>(26,0));
         for(int i=0; i<n; i++)
         {
             for(int k = words[i].size() - 1; k >= 0; k--)
                 table[i][words[i][k] - 'a']++;
         }
         for(int i=0; i<n; i++)
+        {
             for(int j=i+1; j<n; j++)
             {// check collision 
                 bool collision = false;
