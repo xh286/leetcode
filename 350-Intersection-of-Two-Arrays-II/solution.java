bb5e4942c67860562b4549b76a3e51370b6eb412
xh286 <xh286@nyu.edu>
2016-05-29 12:16:12 -0400

    [350. Intersection of Two Arrays II][Compile Error]committed by LeetCode Extension

--- 350-Intersection-of-Two-Arrays-II/solution.java
+++ 350-Intersection-of-Two-Arrays-II/solution.java
@@ -1,32 +1,57 @@
-import java.util.HashSet;
+import java.lang.*
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        if(nums2.length < n)
-        {
-            n = nums2.length;
-        }
-        int[] a = new int[n];
-        int i = 0;
-        HashSet<Integer> nums1_set = new HashSet<Integer>(); // empty set
+        int[] output = new int[Math.min(nums1.length, nums2.length)]; // output at most as long as the shorter of nums1, nums2.
+        // Build the frequency table of both nums1 and nums2
+        HashMap<Integer, Integer> nums1_map = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> nums2_map = new HashMap<Integer, Integer>();
         for(int x: nums1)
         {
-            nums1_set.add(x); // if duplicate don't add.
-        }
-        for(int y: nums2)
-        {
-            if(nums1_set.contains(y))
+            if(nums1_map.containsKey(x))
             {
-                a[i] = y;
-                i = i + 1;
+                nums1_map.put(x, nums1_map.getValue(x)+1);
+            }
+            else
+            {
+                nums1_map.put(x,1);
             }
         }
-        n = i;
-        int[] b = new int[n]; // output, copy a[0:i-2] to b.
-        for(i = 0; i < n; i=i+1)
+        for(int x: nums2)
         {
-            b[i] = a[i];
+            if(nums2_map.containsKey(x))
+            {
+                nums2_map.put(x, nums2_map.getValue(x)+1);
+            }
+            else
+            {
+                nums2_map.put(x,1);
+            }
         }
-        return b;
+        // frequency tables ready, now iterate the smaller map and lookup in larger map to get output
+        int i = 0;
+        for(Map.Entry<Integer,Integer> it : nums1_map)
+        {
+            int k = it.getKey();
+            if(nums2_map.containsKey(k)) // intersection found
+            {
+                int d = Math.min(nums1_map.get(k), nums2_map.get(k));
+                // now output d times
+                for(int j = 0; j < d; j++)
+                {
+                    output[i] = k;
+                    i++;
+                }
+            }
+        }
+        // set output[i - end] to null.
+        for(; i < output.length; i++)
+        {
+            output[i] = null;
+        }
+        return output;
     }
 }
