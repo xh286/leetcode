9aff024e621779d26d63a6d8e865a896dc9eb898
xh286 <xh286@nyu.edu>
2016-05-28 21:51:23 -0400

    [217. Contains Duplicate][Compile Error]committed by LeetCode Extension

--- 217-Contains-Duplicate/solution.java
+++ 217-Contains-Duplicate/solution.java
@@ -1,6 +1,14 @@
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-        
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<int> detect_dup = new HashSet<int>(); //empty
+        for(int x: nums)
+        {
+            if(detect_dup.contains(x))
+                return true;
+            else
+                detect_dup.add(x);
+        }
+        return false;
     }
-};
+}
