d818daffd75d60d37ebbd1b4f9ef8354bf67a65c
xh286 <xh286@nyu.edu>
2016-05-28 21:12:01 -0400

    [260. Single Number III][Accepted]committed by LeetCode Extension

---
+++ 260-Single-Number-III/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        // The xor result is the different bits between the two numbers
+        // use one bit to partition entire set into two subsets, then each subset xor result is a number.
+        int n = nums.size();
+        int bit_pattern = 0;
+        for(int i=0; i < n; i++)
+        {
+            bit_pattern ^= nums[i];
+        }
+        // find one set bit out of bit_pattern
+        int pos = 0;
+        while(0 == (bit_pattern & 0x1))
+        {
+            bit_pattern >>= 1;
+            pos++;
+        }
+        // bit [pos] of bit_pattern was set
+        vector<int> output(2,0);
+        for(int i=0; i < n; i++)
+        {
+            if(nums[i] & (1<<pos)) // if bit [pos] is set, put into same set as num1
+            {
+                output[0] ^= nums[i];
+            }
+            else
+            {
+                output[1] ^= nums[i];
+            }
+        }
+        return output;
+    }
+};
