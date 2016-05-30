bece3139adfe7a2f661f2483de3aaef8d9ffb18d
xh286 <xh286@nyu.edu>
2016-05-29 20:46:16 -0400

    [13. Roman to Integer][Accepted]committed by LeetCode Extension

--- 13-Roman-to-Integer/solution.cpp
+++ 13-Roman-to-Integer/solution.cpp
@@ -1,22 +1,4 @@
 class Solution {
-private:
-    int romanToInt_s(char a)
-    {
-        int i;
-        switch(a)
-        {
-            case 'I': i = 1; break;
-            case 'V': i = 5; break;
-            case 'X': i = 10; break;
-            case 'L': i = 50; break;
-            case 'C': i = 100; break;
-            case 'D': i = 500; break;
-            case 'M': i = 1000; break;
-            default:  i = 0;
-        }
-        return i;
-    }
-    
 public:
     int romanToInt(string s) {
         int n = s.size();
@@ -25,7 +7,17 @@ public:
         int next_val = 0;
         for(int i=n-1; i>=0; i--)
         {
-            this_val = romanToInt_s(s[i]);
+            switch(s[i])
+            {
+            case 'I': this_val = 1; break;
+            case 'V': this_val = 5; break;
+            case 'X': this_val = 10; break;
+            case 'L': this_val = 50; break;
+            case 'C': this_val = 100; break;
+            case 'D': this_val = 500; break;
+            case 'M': this_val = 1000; break;
+            default:  this_val = 0;
+            }
             if(this_val<next_val)
                 output -= this_val;
             else
