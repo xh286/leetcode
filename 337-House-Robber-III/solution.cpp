9e89be661bf47298e9e2ab31102dff4b67958e54
xh286 <xh286@nyu.edu>
2016-05-30 16:34:30 -0400

    [337. House Robber III][Compile Error]committed by LeetCode Extension

--- 337-House-Robber-III/solution.cpp
+++ 337-House-Robber-III/solution.cpp
@@ -9,26 +9,52 @@
  */
 class Solution {
 public:
-    int rob_withroot(TreeNode* p)
-    {
-        int result = p->val;
-        if(p->left) result += rob_withoutroot(p->left);
-        if(p->right) result += rob_withoutroot(p->right);
-        return result;
-    }
-    int rob_withoutroot(TreeNode* p)
-    {
-        int result = 0;
-        if(p->left) result += max(rob_withroot(p->left), rob_withoutroot(p->left));
-        if(p->right) result += max(rob_withroot(p->right), rob_withoutroot(p->right));
-        return result;
-    }
     int rob(TreeNode* root) {
         // Optimal subtructure. For a subtree p with left & right, define f(p) as solution with p included, g(p) as solution without p included.
         // f(p) = p->val + g(left)+g(right). g(p) = max(f(left), g(left)) + max(f(right), g(right)).
         // For each node, we need to calculate f & g. How to store them? How to traverse the tree? Need post-order. Need bottom-up.
         // recursive solution is trivial and overlapping. This is DP.
-        if(root) return max(rob_withroot(root), rob_withoutroot(root));
-        else return 0;
+        if(root == NULL) return 0;
+        // Post order DFS. Update parent value when returning from left subtree and right subtree.
+        stack<TreeNode* > trav_stack;
+        stack<int> maxval_wroot;
+        stack<int> maxval_woroot;
+        bool subtree_visted = false;
+        TreeNode* subtree = root;
+        while(true)
+        {
+            if(subtree && !subtree_visited) // p_visited protection necessary for post-order DFS. Set to true when pop p from stack.
+            {
+                trav_stack.push(subtree); // push left chains by pre-order
+                maxval_wroot.push(subtree->val);
+                maxval_woroot.push(0);
+                subtree = subtree->left;
+                continue;
+            }
+            // Subtree is either NULL or visited. Go back to stack top.
+            if(trav_stack.empty()) break;
+            TreeNode* parent = trav_stack.top;
+            // subtree is always left or right child of stack top. If right child, then done.
+            if(subtree == parent->right)
+            {
+                if(subtree == parent->left) // parent is leaf
+                {
+                }
+                trav_stack.pop(); // pop parent, process here
+                int child_f = maxval_wroot.top();
+                int child_g = maxval_woroot.top();
+                maxval_wroot.pop();
+                maxval_woroot.pop();
+                maxval_wroot.top() += child_g;
+                maxval_woroot.top() += max(child_f, child_g);
+                subtree = parent; // go up one level
+                subtree_visited = true;
+            }
+            else // left child is done
+            {
+                subtree = trav_stack.top()->right;
+                subtree_visited = false;
+            }
+        }
     }
 };
