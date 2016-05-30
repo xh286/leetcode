2c24c5f00293bc28ceac2b7ad62d9363b25dc395
xh286 <xh286@nyu.edu>
2016-05-30 17:08:33 -0400

    [20. Valid Parentheses][Compile Error]committed by LeetCode Extension

---
+++ 20-Valid-Parentheses/solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> test_stack;
+        int n = s.size();
+        for(i=0; i<n; i++)
+        {
+            switch(s[i])
+            {
+                case '(':
+                case '[':
+                case '{':
+                    test_stack.push(s[i]); break;
+                case ')':
+                    if(test_stack.top() != '(')
+                        return false;
+                    else
+                        test_stack.pop();
+                    break;
+                case ']':
+                    if(test_stack.top() != '[')
+                        return false;
+                    else
+                        test_stack.pop();
+                    break;
+                case '}':
+                    if(test_stack.top() != '{')
+                        return false;
+                    else
+                        test_stack.pop();
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return test_stack.empty();
+    }
+};
