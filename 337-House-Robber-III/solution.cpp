157daff509f142d007adfa82964b41bb19a4da6a
xh286 <xh286@nyu.edu>
2016-05-30 15:51:43 -0400

    [337. House Robber III][Time Limit Exceeded]committed by LeetCode Extension

--- 337-House-Robber-III/solution.cpp
+++ 337-House-Robber-III/solution.cpp
@@ -11,17 +11,24 @@ class Solution {
 public:
     int rob_withroot(TreeNode* p)
     {
-        return p->val + rob_withoutroot(p->left) + rob_withoutroot(p->right);
+        int result = p->val;
+        if(p->left) result += rob_withoutroot(p->left);
+        if(p->right) result += rob_withoutroot(p->right);
+        return result;
     }
     int rob_withoutroot(TreeNode* p)
     {
-        return max(rob_withroot(p->left), rob_withoutroot(p->left)) + max(rob_withroot(p->right), rob_withoutroot(p->right)); 
+        int result = 0;
+        if(p->left) result += max(rob_withroot(p->left), rob_withoutroot(p->left));
+        if(p->right) result += max(rob_withroot(p->right), rob_withoutroot(p->right));
+        return result;
     }
     int rob(TreeNode* root) {
         // Optimal subtructure. For a subtree p with left & right, define f(p) as solution with p included, g(p) as solution without p included.
         // f(p) = p->val + g(left)+g(right). g(p) = max(f(left), g(left)) + max(f(right), g(right)).
         // For each node, we need to calculate f & g. How to store them? How to traverse the tree? Need post-order. Need bottom-up.
         // recursive solution is trivial and overlapping. This is DP.
-        return max(rob_withroot(root), rob_withoutroot(root));
+        if(root) return max(rob_withroot(root), rob_withoutroot(root));
+        else return 0;
     }
 };
