4837d8b059559ce47633cf3987dccaf53219b96f
xh286 <xh286@nyu.edu>
2016-05-29 20:09:46 -0400

    [318. Maximum Product of Word Lengths][Accepted]committed by LeetCode Extension

--- 318-Maximum-Product-of-Word-Lengths/solution.cpp
+++ 318-Maximum-Product-of-Word-Lengths/solution.cpp
@@ -4,39 +4,19 @@ public:
         // brute force solution as first step
         int n = words.size();
         int maxproduct = 0;
-        vector<int> l_words; // lengths of (original) words;
-        vector<vector<int> > table(n, vector<int>(26,0));
-        vector<string> c_words(n); // converted words. IN each word, characters are unique and ascending.
+        vector<unsigned int> table(n,0);
         for(int i=0; i<n; i++)
         {
             int len = words[i].size();
             for(int k = 0; k < len; k++)
-                table[i][words[i][k] - 'a']++;
-            for(int k = 0; k < 26; k++)
-                if(table[i][k] > 0)
-                    c_words[i].push_back( (char)('a'+k));
+                table[i] |= 1 << (words[i][k] - 'a');
         }
         
         for(int i=0; i<n; i++)
         {
             for(int j=i+1; j<n; j++)
             {// check collision 
-                bool collision = false;
-                int len1 = c_words[i].size();
-                int len2 = c_words[j].size();
-                for(int p = 0, q = 0; p < len1 && q < len2;)
-                {
-                    if(c_words[i][p] < c_words[j][q])
-                        p++;
-                    else if(c_words[i][p] > c_words[j][q])
-                        q++;
-                    else // (c_words[i][p] == c_words[j][q]) // collision
-                    {
-                        collision = true;
-                        break;
-                    }
-                }
-                if(!collision)
+                if((table[i] & table[j]) == 0)
                 {
                     int product = words[i].size() * words[j].size();
                     if(product > maxproduct) 
