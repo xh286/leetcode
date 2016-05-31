d37e20218051c0d4089d9856bd3b3aabdecc7f50
xh286 <xh286@nyu.edu>
2016-05-31 16:28:07 -0400

    [345. Reverse Vowels of a String][Accepted]committed by LeetCode Extension

--- 345-Reverse-Vowels-of-a-String/solution.cpp
+++ 345-Reverse-Vowels-of-a-String/solution.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        vector<char> v; // stack of vowels
+        string v; // stack of vowels
         string r = s; // result
         int n = s.size();
         for(int i=0; i<n; i++)
         {
-            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
                 v.push_back(s[i]);
         }
         for(int i=0; i<n; i++)
         {
-            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
             {
                 r[i] = v.back();
-                v.pop_back(s[i]);
+                v.pop_back();
             }
         }
         return r;
