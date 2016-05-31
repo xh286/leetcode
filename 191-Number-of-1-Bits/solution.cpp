45c627d4a88c4564043f6f745f8c9e3e80408d6b
xh286 <xh286@nyu.edu>
2016-05-30 20:57:08 -0400

    [191. Number of 1 Bits][Compile Error]committed by LeetCode Extension

--- 191-Number-of-1-Bits/solution.cpp
+++ 191-Number-of-1-Bits/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int r = 0;
-        while(int i=0; i<n; i++)
+        while(int i=0; i<32; i++)
         {
             if(n&1)
                 r++;
