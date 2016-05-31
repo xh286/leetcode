2440d0b5ab9a50e56cabb8e67e11a8ba7723a9a9
xh286 <xh286@nyu.edu>
2016-05-31 16:52:18 -0400

    [198. House Robber][Accepted]committed by LeetCode Extension

--- 198-House-Robber/solution.cpp
+++ 198-House-Robber/solution.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
         // Define value of rob(n) as f(n). f(0)= 0, f(1) = nums[0], f(n) = max(f(n-1), f(n-2)+nums[n-1]).
         // No need for vector storage, because only need last two values of f. Call them: f, f_1less, f_2less.
         int f_2less = 0;
         int f_1less = nums[0];
         int f;
-        int n = nums.size(); 
+        
         for(int i=2; i <= n; i++) // calculate f(i). f_1less stores f(i-1), f_2less stores f(i-2).
         {
             f = max(f_1less, f_2less+nums[i-1]);
