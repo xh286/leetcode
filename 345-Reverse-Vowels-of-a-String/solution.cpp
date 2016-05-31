c96597d67ed0013d2e6617633dbf5ae7b0dec5ef
xh286 <xh286@nyu.edu>
2016-05-31 16:23:44 -0400

    [345. Reverse Vowels of a String][Compile Error]committed by LeetCode Extension

---
+++ 345-Reverse-Vowels-of-a-String/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        string v; // stack of vowels
+        string r = s; // result
+        int n = s.size();
+        for(int i=0; i<n; i++)
+        {
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                v.push_back(s[i]);
+        }
+        for(int i=0; i<n; i++)
+        {
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            {
+                r[i] = v.back();
+                v.pop_back(s[i]);
+            }
+        }
+        return r;
+    }
+};
