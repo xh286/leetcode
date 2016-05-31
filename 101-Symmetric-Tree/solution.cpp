bf16061eaf3da3558229ac86b150c931070aa165
xh286 <xh286@nyu.edu>
2016-05-31 19:36:59 -0400

    [101. Symmetric Tree][Compile Error]committed by LeetCode Extension

--- 101-Symmetric-Tree/solution.cpp
+++ 101-Symmetric-Tree/solution.cpp
@@ -9,12 +9,15 @@
  */
 class Solution {
 public:
+    bool twoTreesSymmetric(TreeNode* p, TreeNode* p)
+    {
+        if(!p && !q) return true;
+        if( (!p && q) || (p && !q) ) return false;
+        return(twoTreesSymmetric(p->left, q->right) && twoTreesSymmetric(p->right, q->left));
+    }
     bool isSymmetric(TreeNode* root) {
         // First recursive solution. Good reference for iterative solution too.
         if(root == NULL) return true;
-        if(root->left && !root->right) return false;
-        if(!root->left && root->right) return false;
-        bool left_right_equal = (root->left->val == root->right->val);
-        return ( left_right_equal && isSymmetric(root->left) && isSymmetric(root->right) );
+        return(twoTreesSymmetric(root->left, root->right));
     }
 };
