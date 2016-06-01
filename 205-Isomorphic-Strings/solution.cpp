c7674d33f4e0ab81fcca4d88d54a172ae5959b59
xh286 <xh286@nyu.edu>
2016-06-01 16:47:10 -0400

    [205. Isomorphic Strings][Compile Error]committed by LeetCode Extension

--- 205-Isomorphic-Strings/solution.cpp
+++ 205-Isomorphic-Strings/solution.cpp
@@ -3,7 +3,7 @@ public:
     bool isIsomorphic(string s, string t) {
         // Many rules: 1. All occurrences of a character must be replaced with same character. 2. Cannot replace two different characters with same character. So: same->same, different->different.
         // Solution: use a hash map since it's not clear which character set is used, UTF-8? ASCII? lower case alphabets?
-        unordered_map<char> char_map;  // On Linux, char will be UTF-8 by default.
+        unordered_map<char, char> char_map;  // On Linux, char will be UTF-8 by default.
         int n = s.size();
         for(int i=0; i<n; i++)
         {
