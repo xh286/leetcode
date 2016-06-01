e78d9df46f0272d20326c25c4e421396ea7b9e6f
xh286 <xh286@nyu.edu>
2016-05-31 20:13:46 -0400

    [66. Plus One][Accepted]committed by LeetCode Extension

--- 66-Plus-One/solution.cpp
+++ 66-Plus-One/solution.cpp
@@ -8,8 +8,9 @@ public:
         {
             if(num_to_add > 0) // add the num to this digit
             {
-                digits[i] = (digits[i] + num_to_add) % 10;
-                if( (digits[i] + num_to_add) >= 10 )
+                int temp = digits[i] + num_to_add;
+                digits[i] = temp % 10;
+                if( temp >= 10 )
                     num_to_add = 1;
                 else
                     num_to_add = 0;
@@ -20,7 +21,7 @@ public:
         // if i == -1, overflow, in that case, insert "1" at the beginning
         if(num_to_add > 0)
         {
-            digits.insert(0, num_to_add);
+            digits.insert(digits.begin(), num_to_add);
         }
         return digits;
     }
