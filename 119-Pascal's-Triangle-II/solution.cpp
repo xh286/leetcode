81bd31909087016779efd5df28eb43bea2acae9c
xh286 <xh286@nyu.edu>
2016-05-31 23:27:12 -0400

    [119. Pascal's Triangle II][Accepted]committed by LeetCode Extension

--- 119-Pascal's-Triangle-II/solution.cpp
+++ 119-Pascal's-Triangle-II/solution.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> getRow(int rowIndex) {
         // kth row has k+1 entries. Easy to miss is 0th row has 1 entry (1), so 0 is also valid input.
         if (rowIndex < 0) return vector<int>();//invalid input
-        if (rowIndex == 0) return vector<int>(1,1);//return {1}
         vector<vector<int> > a(2,vector<int>(rowIndex+1,1)); // initialize all entries to 1.
         // we use ping-pong buffer to avoid excessive copying
         // For k == 1, we already have result in a[0]: {1, 1}. Now we calculate k == 2 result in a[1].
@@ -13,11 +12,11 @@ public:
         for(int k = 2; k <= rowIndex; k++)// k is counter for 2 : rowIndex
         {
             int in = k&1;
-            int out = (k-1)&1;
+            int out = (k+1)&1;
             // Now we derive a[out] from a[in]. The number of valid entries in a[in] == k. The number of entries to derive in a[out] is [1 ... k-1]. Entry k is always 1. a[out] always has k+1 valid entries.
             for(int i=1; i < k; i++)
                 a[out][i] = a[in][i] + a[in][i-1];
         }
-        return a[(rowIndex-1)&1];
+        return a[(rowIndex+1)&1];
     }
 };
