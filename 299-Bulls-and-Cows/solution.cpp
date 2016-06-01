158eec0163c080b239b9089431bef30adf84a187
xh286 <xh286@nyu.edu>
2016-06-01 15:59:21 -0400

    [299. Bulls and Cows][Compile Error]committed by LeetCode Extension

---
+++ 299-Bulls-and-Cows/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int n = secret.size();
+        vector<int> b_s(10,0); // 10 buckets for 10 digits 0 - 9.
+        vector<int> b_g(10,0);
+        // go through secret & guess simultaneously, count bulls and count all others into their buckets.
+        int bulls = 0;
+        for(int i=0; i<n; i++)
+        {
+            if(secret[i] == guess[i])
+            {
+                bulls++;
+            }
+            else
+            {
+                b_s[secret[i] - '0']++;
+                b_g[guess[i] - '0']++;
+            }
+        }
+        int cows = 0;
+        for(int i=0; i<10; i++)
+        {
+            cows += min(b_s[i], b_g[i]);
+        }
+        // Now generate output. Note that bulls and cows can be multiple digits
+        string s;
+        s << bulls << 'A' << cows << 'B';
+        return s;
+    }
+};
