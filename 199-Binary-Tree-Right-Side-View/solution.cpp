ea6fb16d8a8efd1ab0436f8c6e7ef96176975e0e
xh286 <xh286@nyu.edu>
2016-06-24 12:24:57 -0400

    [199. Binary Tree Right Side View][Compile Error]committed by LeetCode Extension

---
+++ 199-Binary-Tree-Right-Side-View/solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    void rlDFS(TreeNode* root, vector<int>& r, vector<int>& v, int level)
+    {
+        if(!root) return;
+        if(level > v.size())
+        {
+            v.push_back(true);
+            r.push_back(root->val);
+        }
+        else
+        {
+            if(v[level-1] == false)
+            {
+                r[level-1] = root->val;
+                v[level-1] = true;
+            }
+        }
+        rlDFS(root->right, r, v, level+1);
+        rlDFS(root->left, r, v, level+1);
+    }
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        // Do a right-to-left DFS, and remember whether each level already output something.
+        vector<int> r;
+        vector<bool> v;
+        rlDFS(root, r, v, 1);
+        return r;
+    }
+};
