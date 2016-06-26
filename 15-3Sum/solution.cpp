5a07d127c4fc374d3245f4cc4c4ced49266bc8ec
xh286 <xh286@nyu.edu>
2016-06-26 16:47:19 -0400

    [15. 3Sum][Compile Error]committed by LeetCode Extension

---
+++ 15-3Sum/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        vector<vector<int>> r;
+        map<int,int> vm;
+        for(int i=0; i<n; i++)//if duplicates, then use max of index as mapped value
+            vm[nums[i]] = i;
+        
+        // Require that triplet[0]<=triplet[1]<=triplet[2].
+        int n = nums.size();
+        for(int i=0; i<n-2; i++)
+        {
+            if(a[i] > 0) break;
+            // Look in a[i+1...n-1] for a[j] and a[k], j !=k, s.t. a[j]+a[k] = -a[i].
+            for(int j=i+1; j<n-1; j++)
+            {
+                int t_2sum = -a[i]; // target for 2sum
+                if(a[j] > t_2sum/2) break;
+                // find in a hash map the index of triplet[2].
+                int target = t_2sum - a[j];
+                map<int,int>::iterator it = vm.find(target);
+                if(it != vm.end() && it->second > j) // found match
+                {
+                    r.push_back();
+                    r.back().push_back(a[i]);
+                    r.back().push_back(a[j]);
+                    r.back().push_back(it->first);
+                }
+            }
+        }
+        return r;
+    }
+};
