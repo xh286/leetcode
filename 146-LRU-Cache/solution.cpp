cb5b099a8cd9e36442fbeaac086859f10efc4538
xh286 <xh286@nyu.edu>
2016-06-28 17:57:52 -0400

    [146. LRU Cache][Compile Error]committed by LeetCode Extension

--- 146-LRU-Cache/solution.cpp
+++ 146-LRU-Cache/solution.cpp
@@ -1,77 +1,111 @@
 class LRUCache{
-    // cache is (key, value) store, usually using hash map.
-    // Simple case is address mapped, cyclic.
-    // LRU policy: just update timestamp (use a counter here) whenever accessed, then invalidate oldest entry.
-    // Time complexity of naive implementation is O(n). To improve:
-    // Keep a (key, counter) hash map. Keep a (counter, key) map.
-    // New access: update (key, counter) hash map, locate iterator in (counter, key) map with old counter, then update counter value. All these can be done in O(logn) time. 
+    // Improved version. Using a linked list of (key, value, n) nodes pointed to by a hash map.
+    // Number n is transaction number, starting from 0, and increments at every successful get & set.
+    // When approaching overflow, needs reset.
+    // Linked list is managed with a tail pointer, list is circular, tail->next is head.
+    // Each new node corresponds to one cache entry, and address is stored in (key, addr) hash map.
+    // Linked list is sorted by n, so head is most recently accessed entry, tail is least recently accessed entry.
+    // When get, look for addr in hash map, then "access it". This means update n and move the node to tail->next.
+    // When set, if new node, create new one, register in hash map, and "access it".
+    // When set and need eviction, replace tail node content, update hash map key, and then "access" tail node.
 private:
-    unsigned cap;
+    struct ListNode{
+        int key;
+        int value;
+        unsigned n;
+        ListNode* pre;
+        ListNode* next;
+        ListNode(int k = 0, int v = 0, unsigned t = 0, ListNode* p1 = NULL, ListNode* p2 = NULL):
+            key(k), value(v), n(t), pre(p1), next(p2){}
+    }
+    unordered_map<int,ListNode*> key2addr;
+    ListNode* head;
+    ListNode* tail;
+    unsigned t;
     unsigned size;
-    unsigned counter;
-    unordered_map<int,int> k2v;
-    unordered_map<int,unsigned> k2t;
-    map<unsigned,int> t2k;
-    void update_ts(int key, unsigned ts)
+    unsigned cap;
+    
+    insertNodeAfterTail(int key, int value)
     {
-        t2k.erase(k2t[key]);
-        t2k[ts] = key;
-        k2t[key] = ts;
-    }
-    void eraseLRU()
-    {
-        map<unsigned,int>::iterator it = t2k.begin(); // how to handle counter wrap-around???
-        t2k.erase(it);
-        k2t.erase(it->second);
-        k2v.erase(it->second);
-        size--;
-    }
-    void insertNewpair(int key, int value, unsigned ts) // not exception safe
-    {
-        k2v[key] = value;
-        t2k[ts] = key;
-        k2t[key] = ts;
+        ListNode* p = new ListNode(key, value, t, tail, head);
+        if(head == NULL) // empty list, first node
+        {
+            tail = head = p;
+            tail->next = tail->pre = head;
+        }
+        else
+        {
+            head->pre = p;
+            head = p;
+            tail->next = head;
+        }
+        key2addr[key] = p;
+        t++;
         size++;
     }
-    void updatePair(int key, int value, unsigned ts)
+    replaceLRU(int key, int value)
     {
-        k2v[key] = value;
-        update_ts(key,ts);
+        key2addr.erase(tail->key); // erase old key -> tail mapping
+        tail->key = key;
+        tail->value = value;
+        tail->n = t;
+        key2addr[key] = tail; // build new key -> tail mapping
+        // old tail becomes new head. move both head & tail backwards. works even if only 1 node.
+        head = tail;
+        tail = tail->pre;
+        t++;
+    }
+    refreshEntry(ListNode* p, int value)
+    {
+        p->n = t;
+        p->value = value;
+        if(p == tail) // p points to tail, includes capacity=1 case.
+        {
+            head = tail;
+            tail = tail->pre;
+            return;
+        }
+        // p is not tail. Move node pointed by p to head.
+        p->pre->next = p->next;
+        p->next->pre = p->pre;
+        p->pre = tail;
+        p->next = head;
+        head->pre = p;
+        tail->next = p;
+        head = p;
+        t++;
     }
 public:
-    LRUCache(int capacity) : 
-        cap(capacity), counter(0), size(0)
-        {
+    LRUCache(int capacity) : head(NULL), tail(NULL), t(0), size(0), cap(capacity) {
+        
     }
     
-    int get(int key) { // not thread safe.
-        unordered_map<int,int>::const_iterator it = k2v.find(key);
-        if(it != k2v.end())
+    int get(int key) {
+        unordered_map<int,ListNode*>::const_iterator it = key2addr.find(key);
+        if(it != key2addr.end())
         {
-            update_ts(key, counter);
-            counter++;
-            return it->second;
+            refreshEntry(it->second, it->second->value);
+            return it->second->value;
         }
-        else // in real caches, this should trigger set().
+        else
+        {
             return -1;
+        }
     }
     
-    void set(int key, int value) {// could modify value for a key!!
-        unordered_map<int,int>::const_iterator it = k2v.find(key);
-        if(it != k2v.end())
+    void set(int key, int value) {
+        // Cache value for given key could be updated!
+        unordered_map<int,ListNode*>::const_iterator it = key2addr.find(key);
+        if(it != key2addr.end())
         {
-            updatePair(key, value, counter);
-            counter++;
+            refreshEntry(it->second, value);
         }
         else
         {
             if(size == cap)
-            {
-                eraseLRU(); // this will reduce size by 1.
-            }
-            
-            insertNewpair(key, value, counter); // this will increase size by 1.
-            counter++;
+                replaceLRU(key, value);
+            else
+                insertNodeAfterTail(key, value);
         }
     }
 };
