745d7454cd420b79507cc21a43955007d3eb1477
xh286 <xh286@nyu.edu>
2016-06-29 12:48:35 -0400

    [370. Range Addition][Compile Error]committed by LeetCode Extension

---
+++ 370-Range-Addition/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
+        vector<int> r(n,0);
+        int m = updates.size();
+        for(int i=0; i<m; i++)
+        {
+            int start = min(n-1,max(0,updates[i][0]));
+            int end = min(n-1,max(0,updates[i][1]));
+            int val = updates[i][2];
+            r[start] += val;
+            r[end] -= val;
+        }
+        // convert r to cumsum.
+        for(int i=1; i<n; i++)
+        {
+            r[i] += r[i-1];
+        }
+        return r;
+    }
+};
