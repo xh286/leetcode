8526f0d49f2d08dbedff451ef849f8015e3f4b83
xh286 <xh286@nyu.edu>
2016-06-29 12:50:14 -0400

    [370. Range Addition][Accepted]committed by LeetCode Extension

--- 370-Range-Addition/solution.cpp
+++ 370-Range-Addition/solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
+        int n = length;
         vector<int> r(n,0);
         int m = updates.size();
         for(int i=0; i<m; i++)
@@ -9,7 +10,8 @@ public:
             int end = min(n-1,max(0,updates[i][1]));
             int val = updates[i][2];
             r[start] += val;
-            r[end] -= val;
+            if(end<n-1)
+                r[end+1] -= val;
         }
         // convert r to cumsum.
         for(int i=1; i<n; i++)
