309061ffe0d47902a22dc041407cd7c0a263918a
xh286 <xh286@nyu.edu>
2016-07-03 17:00:23 -0400

    [225. Implement Stack using Queues][Accepted]committed by LeetCode Extension

--- 225-Implement-Stack-using-Queues/solution.cpp
+++ 225-Implement-Stack-using-Queues/solution.cpp
@@ -1,35 +1,31 @@
 class Stack {
+    // Just one queue is good enough. 
 private:
-    vector<queue<int>> queues; // ping-pong
-    int out_idx; // queues[out_idx] for pop, queues[out_idx^1] for other queue that's always empty except when operating.
-    // When push, push new entry into other queue, and append entries in other queue. So output queue always has entries in reversed order.
+    queue<int> q;
 public:
     // Push element x onto stack.
     void push(int x) {
-        int n = queues[out_idx].size();
-        int new_out_idx = out_idx^1;
-        queues[new_out_idx].push(x);
-        for(int i=0; i<n; i++)
+        int n = q.size();
+        q.push(x);
+        while(n--) 
         {
-            queues[new_out_idx].push(queues[out_idx].front());
-            queues[out_idx].pop();
+            q.push(q.front());
+            q.pop();
         }
-        out_idx = new_out_idx;
     }
 
     // Removes the element on top of the stack.
     void pop() {
-        queues[out_idx].pop();
+        q.pop();
     }
 
     // Get the top element.
     int top() {
-        return queues[out_idx].front();
+        return q.front();
     }
 
     // Return whether the stack is empty.
     bool empty() {
-        return queues[out_idx].empty();
+        return q.empty();
     }
-    Stack(): out_idx(0), queues(vector<queue<int>>(2,queue<int>())) {} // constructor
 };
