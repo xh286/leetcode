5220aba3b040e8862dcaa2387415822486d9a312
xh286 <xh286@nyu.edu>
2016-07-03 22:47:09 -0400

    [33. Search in Rotated Sorted Array][Accepted]committed by LeetCode Extension

--- 33-Search-in-Rotated-Sorted-Array/solution.cpp
+++ 33-Search-in-Rotated-Sorted-Array/solution.cpp
@@ -4,25 +4,25 @@ public:
         // no dup makes it much easier
         // First locate original begin & end. That's where it starts to drop. Then determine which part to search. Then binary search again.
         int n = nums.size();
+        if(n==0) return -1;
         int low = 0;
         int high = n-1;
         int mid;
-        // search for begin
-        while(low<high) // we always have nums[high] < nums[low], and begin is in between.
-        {
-            mid = low +(high-low)/2;
-            if(nums[mid]>nums[low]) low = mid + 1; // mid cannot be begin
-            else if(nums[mid]<nums[high]) high = mid; // 
-        }
-        int begin = low;
-        if(begin>0) assert(nums[begin-1]>nums[begin]);
-        if(begin==0)
-        {
-            low = 0;
-            high = n-1;
-        }
-        else
-        {
+        int begin;
+        // rotated or not?
+        if(nums[low] <= nums[high]) // contains n==1 case
+            begin = 0;
+        else // begin != 0
+        {// search for begin
+            while(high-low>1) // we always have nums[low] > nums[high], and begin is in between.
+            {
+                mid = low +(high-low)/2; // if high-low < 2, mid could overlap with low & high.
+                if(nums[mid]>nums[low]) low = mid; // mid cannot be begin
+                else if(nums[mid]<nums[high]) high = mid; // 
+            }
+            begin = high;
+            assert(nums[begin-1]>nums[begin]);
+            // Got begin
             if(target>nums[n-1])
             { 
                 low = 0; 
@@ -39,8 +39,10 @@ public:
         while(low<=high)
         {
             mid = low +(high-low)/2;
-            if(target>nums[mid]) low = mid + 1;
-            else if(target<nums[mid]) high = mid - 1;
+            if(target>nums[mid]) 
+                low = mid + 1;
+            else if(target<nums[mid]) 
+                high = mid - 1;
             else
                 return mid;
         }
