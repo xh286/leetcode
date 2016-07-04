7e371ba474e712ff7ab9dd010cf57bbf2ac88761
xh286 <xh286@nyu.edu>
2016-07-04 15:38:06 -0400

    [15. 3Sum][Time Limit Exceeded]committed by LeetCode Extension

--- 15-3Sum/solution.cpp
+++ 15-3Sum/solution.cpp
@@ -1,40 +1,28 @@
 class Solution {
-
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        std::sort(nums.begin(), nums.end());
         vector<vector<int>> r;
-        map<Triplet, 
-        map<int,int> vm;
-        for(int i=0; i<n; i++)//if duplicates, then use max of index as mapped value
-            vm[nums[i]] = i;
-        // Require that triplet[0]<=triplet[1]<=triplet[2].
-        
+        int n = nums.size();
+        if(n<3) return r;
+        std::sort(nums.begin(), nums.end());
+        // For sorted array, 2sum is linear complexity without hash map.
+        // Require that t1<=t2<=t3.
         for(int i=0; i<n-2; i++)
         {
-            if(nums[i] > 0) break;
-            // Look in a[i+1...n-1] for a[j] and a[k], j !=k, s.t. a[j]+a[k] = -a[i].
-            for(int j=i+1; j<n-1; j++)
+            int target = -nums[i];
+            int j = i + 1;
+            int k = n - 1;
+            while(j<k)
             {
-                int t_2sum = -nums[i]; // target for 2sum
-                if(nums[j] > t_2sum/2) break;
-                // find in a hash map the index of triplet[2].
-                int target = t_2sum - nums[j];
-                map<int,int>::iterator it = vm.find(target);
-                if(it != vm.end() && it->second > j) // found match
+                int sum = nums[j] + nums[k];
+                if(sum < target) j++;
+                else if(sum > target) k--;
+                else // hit
                 {
-                    if(r.empty() || !(r.back()[0] == nums[i] && r.back()[1] == nums[j]))
-                    {
-                    r.push_back(vector<int>());
-                    r.back().push_back(nums[i]);
-                    r.back().push_back(nums[j]);
-                    r.back().push_back(it->first);
-                    }
+                    r.push_back({i,j,k});
                 }
             }
         }
-        
         return r;
     }
 };
