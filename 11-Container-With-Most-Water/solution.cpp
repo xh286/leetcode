0192bd987d387f759f0aa1fd52022cbd56db3816
xh286 <xh286@nyu.edu>
2016-07-04 17:39:24 -0400

    [11. Container With Most Water][Accepted]committed by LeetCode Extension

---
+++ 11-Container-With-Most-Water/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        // Total n^2 pairs, area for each pair is min*dist O(1), so naive solution n^2.
+        // DP: no idea. Complex. 
+        // Plotting examples: found: from left side, only need to track an increasing sequence. Same from right side.
+        // So, probably faster than n^2. Worst case is: 1,2,3,...k,...3,2,1.
+        // But looking at this we see that any <= need to be skipped.
+        int n = height.size();
+        int max_area = 0;
+        int low = 0;
+        int high = n-1;
+        while(low<high)
+        {
+            int h = min(height[low],height[high]);
+            int area = (high-low)*h;
+            if(area>max_area) max_area = area;
+            while(low<high && height[low]<=h) low++;
+            while(low<high && height[high]<=h) high--;
+        }
+        return max_area;
+    }
+};
