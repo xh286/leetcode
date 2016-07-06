332e37ebd7d836ed0823898fb6db2169062a5d71
xh286 <xh286@nyu.edu>
2016-07-06 18:22:28 -0400

    [139. Word Break][Compile Error]committed by LeetCode Extension

---
+++ 139-Word-Break/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string>& wordDict) {
+        for(auto it=s.begin(); it!=s.end(); it++)
+        {
+            string t(s.begin(), it+1);
+            if(wordDict.find(t)!=wordDict.end() && wordBreak(string(it+1,wordDict))) 
+                return true;
+            }
+        }
+    }
+};
