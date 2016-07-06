7847f9c8f68fbaff0f9799e18ce8fb0451003b3e
xh286 <xh286@nyu.edu>
2016-07-06 18:27:04 -0400

    [139. Word Break][Time Limit Exceeded]committed by LeetCode Extension

--- 139-Word-Break/solution.cpp
+++ 139-Word-Break/solution.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     bool wordBreak(string s, unordered_set<string>& wordDict) {
+        if(s.size()==0) return true;
         for(auto it=s.begin(); it!=s.end(); it++)
         {
             string t(s.begin(), it+1);
             if(wordDict.find(t)!=wordDict.end())
                 if(wordBreak(string(it+1,s.end()),wordDict))
-                return true;
+                    return true;
         }
+        return false;
     }
 };
