1f975816847ec75d4eed2ba393c9e131880a6d06
xh286 <xh286@nyu.edu>
2016-07-17 17:39:23 -0400

    [302. Smallest Rectangle Enclosing Black Pixels][Time Limit Exceeded]committed by LeetCode Extension

--- 302-Smallest-Rectangle-Enclosing-Black-Pixels/solution.cpp
+++ 302-Smallest-Rectangle-Enclosing-Black-Pixels/solution.cpp
@@ -4,30 +4,37 @@ class Solution {
       Image has '0' or '1'. x for row, y for column.
     */
 private:
-    void visitPixel(vector<vector<char>>& image, int x, int y, int& x_min, int& x_max, int& y_min, int& y_max)
+    int x_min;
+    int y_min;
+    int x_max;
+    int y_max;
+    int m;
+    int n;
+    void visitPixel(vector<vector<char>>& image, int x, int y)
     {
-        int m = image.size();
-        int n = image[0].size();
         x_min = min(x, x_min);
         y_min = min(y, y_min);
         x_max = max(x, x_max);
         y_max = max(y, y_max);
-        if(x-1>=0 && image[x-1][y]=='1') visitPixel(image, x-1, y, x_min, x_max, y_min, y_max);
-        if(x+1<m && image[x+1][y]=='1') visitPixel(image, x+1, y, x_min, x_max, y_min, y_max);
-        if(y-1>=0 && image[x][y-1]=='1') visitPixel(image, x, y-1, x_min, x_max, y_min, y_max);
-        if(y+1<n && image[x][y+1]=='1') visitPixel(image, x, y+1, x_min, x_max, y_min, y_max);
+        image[x][y] = '0';
+        if(x-1>=0 && image[x-1][y]=='1') visitPixel(image, x-1, y);
+        if(x+1<m && image[x+1][y]=='1')  visitPixel(image, x+1, y);
+        if(y-1>=0 && image[x][y-1]=='1') visitPixel(image, x, y-1);
+        if(y+1<n && image[x][y+1]=='1')  visitPixel(image, x, y+1);
+        image[x][y] = '1';
     }
 public:
     int minArea(vector<vector<char>>& image, int x, int y) {
-        int m = image.size();
-        int n = image[0].size();
-        if(m<=0 || n<=0) return 0;
-        int x_min = x;
-        int x_max = x;
-        int y_min = y;
-        int y_max = y;
+        m = image.size();
+        if(m==0) return 0;
+        n = image[0].size();
+        if(n==0) return 0;
+        x_min = INT_MAX;
+        x_max = INT_MIN;
+        y_min = INT_MAX;
+        y_max = INT_MIN;
         // assert(image[x][y] == '1');
-        visitPixel(image, x, y, x_min, x_max, y_min, y_max);
-        return (x_max-x_min+1)*(y_max-y_min+1);
+        visitPixel(image, x, y);
+        return ((x_max - x_min+1)*(y_max - y_min+1));
     }
 };
