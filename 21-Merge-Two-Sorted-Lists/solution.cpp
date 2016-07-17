01d5bc4299aff6e5c673451d9bc0e4cefc829f4f
xh286 <xh286@nyu.edu>
2016-07-17 09:35:34 -0400

    [21. Merge Two Sorted Lists][Accepted]committed by LeetCode Extension

--- 21-Merge-Two-Sorted-Lists/solution.cpp
+++ 21-Merge-Two-Sorted-Lists/solution.cpp
@@ -9,24 +9,30 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        // Iterative solution minimizing writes. Complex logic, cannot beat simpler, "less efficient" version.
         if(!l1) return l2;
         if(!l2) return l1;
-        ListNode *head, *tail;
-        if(l1->val <= l2->val)
-            {head=tail=l1;l1=l1->next;}
-        else
-            {head=tail=l2;l2=l2->next;}
-        // keep collecting smaller node and splice after tail
-        while(l1&&l2)
+        ListNode *host, *guest;
+        if(l1->val <= l2->val){host = l1; guest = l2;}
+        else{host = l2; guest = l1;}
+        ListNode *p = host;
+        ListNode *q = guest;
+        ListNode *p_next = p->next;
+        ListNode *new_q;
+        // Maintain invariant: find p & p_next pair that satisfy p->val <= q->val <= p_next->val.
+        // Then q keep going until q->next->val > p->next->val, then new_q = q->next. And splice all q nodes between first q and last q inbetween p & p_next.
+        // Requires valid p_next and q.
+        while(p_next && q)
         {
-            if(l1->val <= l2->val)
-                {tail->next = l1; l1=l1->next;}
-            else
-                {tail->next = l2; l2=l2->next;}
-            tail = tail->next;
+            if(p_next->val < q->val)
+            {p = p->next; p_next = p->next; continue;}
+            p->next = q;
+            while(q->next && q->next->val <= p_next->val) q = q->next;
+            new_q = q->next;
+            q->next = p_next;
+            q = new_q;
         }
-        if(!l1) tail->next = l2;
-        if(!l2) tail->next = l1;
-        return head;
+        if(!p_next) p->next = q;
+        return host;
     }
 };
